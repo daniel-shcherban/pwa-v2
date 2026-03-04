fdf8e4f2ffb5d9f096955de2ba4d15b0d980ed09
Danik499 <danikshcherban499@gmail.com>
2026-03-04 17:07:12 +0200

    Updates

--- assets/index-DBt8BHJW.js
+++ assets/index-DBt8BHJW.js
@@ -16546,9 +16546,27 @@ async function fetchTestItems() {
   return res.json();
 }
 function RemoteTodos() {
+  const [onlineStatus, setOnlineStatus] = host__loadShare__react__loadShare__.useState(true);
+  host__loadShare__react__loadShare__.useEffect(() => {
+    window.addEventListener("offline", () => {
+      setOnlineStatus(false);
+    });
+    window.addEventListener("online", () => {
+      setOnlineStatus(true);
+    });
+    return () => {
+      window.removeEventListener("offline", () => {
+        setOnlineStatus(false);
+      });
+      window.removeEventListener("online", () => {
+        setOnlineStatus(true);
+      });
+    };
+  }, []);
   const { data, isLoading, isError } = host__loadShare___mf_0_tanstack_mf_1_react_mf_2_query__loadShare__.useQuery({
     queryKey: ["testItems"],
-    queryFn: fetchTestItems
+    queryFn: fetchTestItems,
+    enabled: onlineStatus
   });
   return /* @__PURE__ */ jsxRuntimeExports.jsxs("div", { children: [
     isLoading && /* @__PURE__ */ jsxRuntimeExports.jsx("div", { children: "Loading test items..." }),
