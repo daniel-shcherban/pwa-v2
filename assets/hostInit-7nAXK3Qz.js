9fac4b247124f2db922986694af8ad3e84855ac5
Danik499 <danikshcherban499@gmail.com>
2026-03-04 16:07:18 +0200

    Updates

--- assets/hostInit-7nAXK3Qz.js
+++ assets/hostInit-7nAXK3Qz.js
@@ -1,6 +1,6 @@
 import { _ as __vitePreload } from './preload-helper-C7Zd8gLW.js';
 
-const remoteEntryPromise = __vitePreload(() => import('./remoteEntry-uudLNHRw.js'),true              ?[]:void 0);
+const remoteEntryPromise = __vitePreload(() => import('./remoteEntry-pLTUk6ej.js'),true              ?[]:void 0);
     // __tla only serves as a hack for vite-plugin-top-level-await. 
     Promise.resolve(remoteEntryPromise)
       .then(remoteEntry => {
--- assets/remoteEntry-pLTUk6ej.js
+++ assets/remoteEntry-pLTUk6ej.js
@@ -10,7 +10,7 @@ const importMap = {
         }
       ,
         "@tanstack/react-query": async () => {
-          let pkg = await __vitePreload(() => import('./index-BcrKM5pd.js'),true              ?[]:void 0);
+          let pkg = await __vitePreload(() => import('./index-B0GBI3lt.js'),true              ?[]:void 0);
           return pkg
         }
       ,
